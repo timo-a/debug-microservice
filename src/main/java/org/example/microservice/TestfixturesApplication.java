b8fd775bc565f2e063cd6d550e92446e754ac305
timo <1398557+timo-a@users.noreply.github.com>
2024-07-23 18:46:21 +0200

    initial commit

---
+++ src/main/java/org/example/microservice/TestfixturesApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfixturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestfixturesApplication.class, args);
+    }
+
+}
